3c00bbe075fc52738b3f0c9b7e4e606e2f2f8551
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2021-02-15 22:20:24 +0800

    Add tasks to gulpfile(3) : optimizing images using gulp-image

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,16 +2,24 @@ import gulp from "gulp"
 import gpug from "gulp-pug";
 import del from "del"
 import ws from "gulp-webserver"
+import img from "gulp-image"
+
 
 const routes = {
     pug: {
         watch:"src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
+    },
+    img : {
+        src:"src/img/*",
+        dest: "build/img"
     }
 }
 
 const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+const image = () => gulp.src(routes.img.src).pipe(img()).pipe(gulp.dest(routes.img.dest))
 
 const watch = () => gulp.watch(routes.pug.watch, pug)
 
@@ -24,7 +32,7 @@ const clean = () => del(["build"])
 
 const prepare = gulp.series([clean])
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, image]);
 
 const live = gulp.series([webserver, watch])
 
