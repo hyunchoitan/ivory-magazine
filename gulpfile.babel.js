9d36cd2758454accafd59ef982f7533ea5619f94
hyunchoitan <60224577+hyunchoitan@users.noreply.github.com>
2021-02-15 21:26:15 +0800

    Add tasks to gulpfile(2) : Watching changes on src, run local webserver using gulp-webserver

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from "gulp"
 import gpug from "gulp-pug";
 import del from "del"
+import ws from "gulp-webserver"
 
 const routes = {
     pug: {
@@ -12,10 +13,19 @@ const routes = {
 
 const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
+const watch = () => gulp.watch(routes.pug.watch, pug)
+
+const webserver = () => gulp.src("build").pipe(ws({
+    livereload: true,
+    open: true
+}))
+
 const clean = () => del(["build"])
 
 const prepare = gulp.series([clean])
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets])
+const live = gulp.series([webserver, watch])
+
+export const dev = gulp.series([prepare, assets, live])
